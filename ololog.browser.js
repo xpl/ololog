915036396c129b338ac8a7e54bc7ecae41a58865
xpl <rocket.mind@gmail.com>
2017-07-22 23:45:38 +0300

    fixed package.json main script path

--- ololog.browser.js
+++ ololog.browser.js
@@ -1,6 +1,6 @@
-/*	This gets compiled by
-		
-		browserify --debug ./ololog.browser.js > ./build/ololog.browser.js	*/
+/*  This gets compiled by
+        
+        browserify --debug ./ololog.browser.js > ./build/ololog.browser.js  */
 
 window.ololog      = require ('./build/ololog.es5')
 window.ansicolor   = require ('ansicolor').nice
