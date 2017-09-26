60a9b621d3fcd764e5aeb0900fb96a03f95e673f
xpl <rocket.mind@gmail.com>
2017-09-26 05:29:46 +0300

    lines from same location now auto-merge into paragraphs ++ sources now reloaded when Mocha restarts under --watch, so stacktraces show correct locations

--- reporter.js
+++ reporter.js
@@ -1,8 +1,10 @@
-const mocha  = require ('mocha')
-    , ansi   = require ('ansicolor')
-    , ololog = require ('ololog')
-    , log    = ololog.configure ({ locate: false })
-    
+const mocha       = require ('mocha')
+    , ansi        = require ('ansicolor')
+    , ololog      = require ('ololog')
+    , StackTracey = require ('stacktracey')
+    , log         = ololog.configure ({ locate: false })
+    , { isBlank } = require ('printable-characters')
+
 /*  ------------------------------------------------------------------------ */
 
 process.on ('uncaughtException',  e => { log.bright.red.error (e) })
@@ -16,85 +18,99 @@ const sleep = ms => new Promise (resolve => setTimeout (resolve, ms))
 
 module.exports = function (runner) {
 
-    mocha.reporters.Base.call (this, runner)
+mocha.reporters.Base.call (this, runner)
 
-    const logImplRender = ololog.impl.render
-    const cursorUp      = '\u001b[1A'
-    
-    runner.on ('suite', ({ title }) => {
+const originalImpl = Object.assign ({}, ololog.impl)
+const cursorUp     = '\u001b[1A'
 
-        if (title) {
-            log.bright (title + ':', '\n')
+StackTracey.resetCache () // when running mocha under --watch, this is needed to re-load the changed sources
+
+runner.on ('suite', ({ title }) => {
+
+    if (title) {
+        log.bright (title + ':', '\n')
+    }
+})
+
+runner.on ('suite end', ({ title, tests }) => {
+
+    if (tests.length) {
+        console.log ('')
+    }
+})
+
+runner.on ('test', test => {
+        
+    test.logBuffer = ''
+
+    let prevLocation
+
+    ololog.impl.render = text => {
+
+        const lines = text.split ('\n')
+        const multiline = lines.length > 1
+
+        const location = new StackTracey ().clean.at (2)
+        const locationChanged = prevLocation && !StackTracey.locationsEqual (location, prevLocation)
+        prevLocation = location
+
+        test.logBuffer += ((locationChanged || multiline) ? '\n' : '') + text + '\n'
+    }
+
+    ;(async () => {
+        
+        const clock = ['◐', '◓', '◑', '◒']
+        
+        console.log ('')
+        
+        for (let i = 0, n = clock.length; !test.state; i++) {
+
+            console.log (ansi.darkGray (cursorUp + clock[i % n] + ' ' + test.title + ' ' + '.'.repeat (i/2 % 5).padEnd (5)))
+            await sleep (100)
         }
-    })
 
-    runner.on ('suite end', ({ title, tests }) => {
+    }) ()
+})
 
-        if (tests.length) {
-            console.log ('')
-        }
-    })
+runner.on ('test end', ({ state = undefined, title, logBuffer, only, verbose = false, parent, ...other }) => {
 
-    runner.on ('test', test => {
-            
-        test.logBuffer = ''
+    ololog.impl.render = originalImpl.render
 
-        ololog.impl.render = text => {
+    if (state) {
+        
+        log.darkGray (cursorUp + { 'passed': '😎', 'failed': '👹' }[state] + ' ',  title, '    ')
+        
+        const onlyEnabled = parent._onlyTests.length
 
-            const lines = text.split ('\n')
-            const multiline = lines.length > 1
-            
-            test.logBuffer += (multiline ? `\n${text}\n` : text) + '\n'
+        while (!verbose && parent) {
+            verbose = parent.verbose
+            parent = parent.parent
         }
 
-        ;(async () => {
-            
-            const clock = ['◐', '◓', '◑', '◒']
-            
-            console.log ('')
-            
-            for (let i = 0, n = clock.length; !test.state; i++) {
+        let show = (onlyEnabled || verbose || (state === 'failed')) && logBuffer
+        if (show) {
 
-                console.log (ansi.darkGray (cursorUp + clock[i % n] + ' ' + test.title + ' ' + '.'.repeat (i/2 % 5).padEnd (5)))
-                await sleep (100)
-            }
+            const sanitized = logBuffer
+                                .split ('\n')
+                                .map (line => isBlank (line) ? '' : line)
+                                .join ('\n')
 
-        }) ()
-    })
-
-    runner.on ('test end', ({ state = undefined, title, logBuffer, only, verbose = false, parent, ...other }) => {
-
-        ololog.impl.render = logImplRender
-
-        if (state) {
-            
-            log.darkGray (cursorUp + { 'passed': '😎', 'failed': '👹' }[state] + ' ',  title, '    ')
-            
-            const onlyEnabled = parent._onlyTests.length
-
-            while (!verbose && parent) {
-                verbose = parent.verbose
-                parent = parent.parent
-            }
-
-            let show = (onlyEnabled || verbose || (state === 'failed')) && logBuffer
-            if (show) {
-                log ('  ', ('\n' + logBuffer.trim () + '\n').replace (/\n\n\n/g, '\n\n'))
-            }
+            log ('  ', '\n' + sanitized.replace (/\n\n\n+/g, '\n\n').trim () + '\n')
         }
-    })
+    }
+})
 
-    runner.on ('fail', (test, err) => {
+runner.on ('fail', (test, err) => {
 
-        if (('actual' in err) && ('expected' in err)) {
-            
-            log.bright.red.error ('[AssertionError] ' + err.message)
-            log.newline ()
-            log.red.error ('\tactual:  ', err.actual)
-            log.green.error ('\texpected:', err.expected)
+    if (('actual' in err) && ('expected' in err)) {
+        
+        log.bright.red.error ('[AssertionError] ' + err.message)
+        log.newline ()
+        log.red.error ('\tactual:  ', err.actual)
+        log.green.error ('\texpected:', err.expected)
 
-        } else {
-            log.bright.red.error (err)            
-        }
-    })
+    } else {
+        log.bright.red.error ('\n', err)            
+    }
+})
 }
